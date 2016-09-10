400f44c37359a2c5e13e3fef025fba4044dc6ce1
David Stanley <davearthur.stanley@gmail.com>
2016-09-10 12:31:14 -0400

    Added command for getting Mac CPU number.

--- pdf2png.sh
+++ pdf2png.sh
@@ -3,8 +3,8 @@
 find . -type f -name '*.pdf' -print0 |
   while IFS= read -r -d '' file
     do
-    # convert -verbose -density 150 "${file}" "${file%.*}.png"
-    echo  convert -verbose -density 150 "${file}" "${file%.*}.png"
+     convert -verbose -density 150 "${file}" "${file%.*}.jpg"
+    echo  convert -verbose -density 150 "${file}" "${file%.*}.jpg"
 
   done
 
