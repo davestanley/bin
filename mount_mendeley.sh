2953ccb1df6d1d330bf73d4667b6536ce38a256a
David Stanley <davearthur.stanley@gmail.com>
2017-10-12 01:39:48 -0400

    Updated mount_mendeley.sh

--- mount_mendeley.sh
+++ mount_mendeley.sh
@@ -1,3 +1,5 @@
 
 sudo mount localhost:/Users/davestanley/GDrive/from_Dropbox/CArticles /Users/davestanley/Dropbox/CArticles
 
+sudo mount localhost:/Users/davestanley/GDrive/from_Dropbox/Startups/Neurofusa/Articles /Users/davestanley/Mendeley_repo/Neurasense/Articles
+
