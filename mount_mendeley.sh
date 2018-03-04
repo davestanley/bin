dd15c2170a9666404ce6b8c9f1d289eae9c054e1
David Stanley <davearthur.stanley@gmail.com>
2018-03-04 18:42:47 -0500

    Updated mount mendely path

--- mount_mendeley.sh
+++ mount_mendeley.sh
@@ -1,5 +1,5 @@
 
 sudo mount localhost:/Users/davestanley/GDrive/from_Dropbox/CArticles /Users/davestanley/Dropbox/CArticles
 
-sudo mount localhost:/Users/davestanley/GDrive/from_Dropbox/Startups/Neurofusa/Articles /Users/davestanley/Mendeley_repo/Neurasense/Articles
+sudo mount localhost:/Users/davestanley/GDrive/from_Dropbox/Startups/Neurofusa/CArticles /Users/davestanley/Mendeley_repo/Neurasense/CArticles
 
