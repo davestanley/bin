5b88c30db264a375e833108fe95bcb773575f309
David Stanley <davearthur.stanley@gmail.com>
2018-10-30 19:31:48 -0400

    Wrote link_github.sh. For now, tuned to mac

---
+++ link_github.sh
@@ -0,0 +1,11 @@
+
+
+#!/bin/bash
+# Script for linking all files in github folder to src folders
+# Usage link_github.sh
+
+link_folders.sh ~/GDrive/from_Dropbox/git ~/src/
+link_folders.sh ~/GDrive_extras/git ~/src2/
+
+
+
