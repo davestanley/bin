07f11c24fb3877d6d3d6b3eb515ff84860f96a55
David Stanley <davearthur.stanley@gmail.com>
2018-10-30 19:58:03 -0400

    Updated link_github.sh to use environment variables

--- link_github.sh
+++ link_github.sh
@@ -4,8 +4,7 @@
 # Script for linking all files in github folder to src folders
 # Usage link_github.sh
 
-link_folders.sh ~/GDrive/from_Dropbox/git ~/src/
-link_folders.sh ~/GDrive_extras/git ~/src2/
+link_folders.sh $PATH_GITHUB $PATH_SRC
 
 
 
